6434feb8f19d0556db10812596da22006d8d15e6
b-runz <bjarke.runz@outlook.dk>
2024-03-23 01:13:54 +0100

    Added missing util

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -16,3 +16,34 @@ export function printName(trip: Trip): string {
     hasher.update(data);
     return hasher.digest('hex');
   }
+
+  import { useState } from 'react';
+
+export const useLocalStorage = (key: string, initialValue: Set<string>): [Set<string>, (value: Set<string> | ((prevState: Set<string>) => Set<string>)) => void] => {
+  const [state, setState] = useState<Set<string>>(() => {
+    // Initialize the state
+    try {
+      const value = window.localStorage.getItem(key);
+      // Check if the local storage already has any values,
+      // otherwise initialize it with the passed initialValue
+      return value ? new Set(JSON.parse(value)) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue; // Return initialValue in case of error
+    }
+  });
+
+  const setValue = (value: Set<string> | ((prevState: Set<string>) => Set<string>)) => {
+    try {
+      // If the passed value is a callback function,
+      // then call it with the existing state.
+      const valueToStore = value instanceof Function ? value(state) : value;
+      window.localStorage.setItem(key, JSON.stringify(Array.from(valueToStore)));
+      setState(valueToStore);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [state, setValue];
+};
